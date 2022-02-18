f99a8dab16790ee4eeb2d4da85bffeb4c11fecf2
avyukti1 <82049896+avyukti1@users.noreply.github.com>
2022-02-18 21:08:48 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello POWERBI Team - Good Morning, How are you ? !")
+    return HttpResponse("Hello POWERBI Team - Good Morning, How are you ? Today we are happy & we are learning Azure & PowerBi!")
